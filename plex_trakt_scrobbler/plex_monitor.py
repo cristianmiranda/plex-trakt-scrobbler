f4e15d4807d21944250cd4ef4f74b078ff2f30cb
Cristian Miranda <crism60@gmail.com>
2016-07-01 01:00:30 -0300

    Movies scrobbling

--- plex_trakt_scrobbler/plex_monitor.py
+++ plex_trakt_scrobbler/plex_monitor.py
@@ -13,6 +13,11 @@ show_name = ''
 season_number = ''
 episode_number = ''
 
+# Processed Movie
+
+imdb_id = ''
+movie_name = ''
+
 # Media common attributes
 
 duration = ''
@@ -97,13 +102,30 @@ def parse_line(config, log_line):
         m = regex.match(log_line)
 
         if m:
-            scrobble_show(config, m.group(1))
+            scrobble(config, m.group(1))
 
     for regex in WATCHED_REGEX:
         m = regex.match(log_line)
 
         if m:
-            mark_show_as_watched(config, m.group(1), m.group(2))
+            mark_as_watched(config, m.group(1), m.group(2))
+
+
+'''
+    Scrobbles a media item against Trakt TV
+
+    @param item: media item ID
+        
+    @return Nothing 
+'''
+def scrobble(config, item):
+
+    plex = Plex(config)
+    media = plex.get_media_metadata_from_library(item)
+    if 'show_id' in media:
+        scrobble_show(config, item)
+    else:
+        scrobble_movie(config, item)
 
 
 '''
@@ -114,12 +136,6 @@ def parse_line(config, log_line):
     @return Nothing 
 '''
 def scrobble_show(config, item):
-    ''' Processes played / unplayed item scrobbling TVShowTime
-
-        :param item: played / unplayed item id
-        :type item: integer 
-        :param played: flag to know if item has been played or unplayed
-        :type played: boolean '''
 
     global show_id
     global show_name
@@ -131,7 +147,7 @@ def scrobble_show(config, item):
     logger = logging.getLogger(__name__)
 
     plex = Plex(config)
-    metadata = plex.get_show_episode_metadata_from_sessions(item, USER_ID)
+    metadata = plex.get_media_metadata_from_sessions(item, USER_ID)
 
     scrobbling = 'stop'
 
@@ -162,6 +178,67 @@ def scrobble_show(config, item):
 
 
 '''
+    Scrobbles a Movie item
+
+    @param item: Movie item ID
+        
+    @return Nothing 
+'''
+def scrobble_movie(config, item):
+
+    global imdb_id
+    global movie_name
+    global duration
+    global progress
+
+    logger = logging.getLogger(__name__)
+
+    plex = Plex(config)
+    metadata = plex.get_media_metadata_from_sessions(item, USER_ID)
+
+    scrobbling = 'stop'
+
+    if metadata:
+        imdb_id = metadata['imdb_id']
+        movie_name = metadata['movie_name']
+        duration = metadata['duration']
+        progress = metadata['progress']
+        scrobbling = metadata['srobbling']
+
+    if imdb_id:
+        movie_label = "{0} - ({1}) - Progress: {2} - Duration: {3}"\
+            .format(movie_name, scrobbling, progress, duration)
+
+        logger.info("Scrobble - {0}".format(movie_label))
+        trakt = Trakt(config)
+        trakt.scrobble_movie(imdb_id, progress, scrobbling)
+
+    if not metadata:
+        imdb_id = ''
+        movie_name = ''
+        duration = ''
+        progress = 0
+
+
+'''
+    Marks a media item as watched
+
+    @param item: media item ID
+
+    @return Nothing
+'''
+def mark_as_watched(config, media_id, user_id):
+    
+    if user_id == USER_ID:
+        plex = Plex(config)
+        media = plex.get_media_metadata_from_library(media_id)
+        if 'show_id' in media:
+            mark_show_as_watched(config, media_id, user_id)
+        else:
+            mark_movie_as_watched(config, media_id, user_id)
+
+
+'''
     Marks a TV Show episode item as watched
 
     @param item: TV Show episode item
@@ -173,12 +250,33 @@ def mark_show_as_watched(config, media_id, user_id):
 
     if user_id == USER_ID:
         plex = Plex(config)
-        metadata = plex.get_show_episode_metadata_from_library(media_id)
+        metadata = plex.get_media_metadata_from_library(media_id)
         if not metadata: return
 
         episode_label = "{0} S{1}E{2}"\
             .format(metadata['show_name'], metadata['season_number'], metadata['episode_number'])
-        logger.info("Mark as watched -  {0}".format(episode_label))
+        logger.info("Mark as watched - {0}".format(episode_label))
 
         trakt = Trakt(config)
         trakt.scrobble_show(metadata['show_name'], metadata['season_number'], metadata['episode_number'], 100, 'stop')
+
+
+'''
+    Marks a Movie item as watched
+
+    @param item: Movie item ID
+
+    @return Nothing
+'''
+def mark_movie_as_watched(config, media_id, user_id):
+    logger = logging.getLogger(__name__)
+
+    if user_id == USER_ID:
+        plex = Plex(config)
+        metadata = plex.get_media_metadata_from_library(media_id)
+        if not metadata: return
+
+        logger.info("Mark as watched - {0}".format(metadata['movie_name']))
+
+        trakt = Trakt(config)
+        trakt.scrobble_movie(metadata['imdb_id'], 100, 'stop')
